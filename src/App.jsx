20ee6db296b1c7589dcbe45c6bcb7b6628894c8a
kuttycop <kuttycop562003@gmail.com>
2024-08-07 20:35:14 +0530

    bmi added

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+  const[height,setHeight]=useState("");
+  const[weight,setWeight]=useState("");
+  const[bmi,setBmi]=useState(null)
+  const[bmiStatus,setBmiStatus]=useState("")
+  const[errorMessage,setErrorMessage]=useState("")
+
+  const bmicalculate=()=>{
+    const isValidHeight=/^\d+$/.test(height)
+    const isValidWeight=/^\d+$/.test(weight)
+    if(isValidHeight&&isValidWeight){
+      const heightInMeters=height/100;
+      const bmiValue=weight/(heightInMeters*heightInMeters);
+      setBmi(bmiValue.toFixed(2));
+      if(bmiValue<18.5){
+        setBmiStatus("Under Weight")
+      }else if(bmiValue>=18.5 && bmiValue< 24.9){
+        setBmiStatus("Normal Weight")
+      }else if(bmiValue>=25 && bmiValue<29.9){
+        setBmiStatus("Over Weight")
+      }else{
+        setBmiStatus("Obese")
+      }
+      setErrorMessage("")
+
+    }else{
+      setBmi(null)
+      setBmiStatus("")
+      setErrorMessage("Please enter valid numeric values for height and weight.!")
+    }
+  };
+  const clearAll=()=>{
+    setHeight("")
+    setWeight("")
+    setBmi(null)
+    setBmiStatus("")
+    setErrorMessage("")
+  };
+  return (
+    <>
+      <div className='bmi-calculator'>
+        <div className="box"></div>
+        <div className="data">
+          <h1>BMI Calculator</h1>
+
+          {errorMessage &&<p className='error'>{errorMessage}</p>}
+          <div className="input-container">
+            <label htmlFor="height">Height (Cm):</label>
+            <input type="text" value={height} id='height' onChange={(e)=>setHeight(e.target.value)}/>
+          </div>
+          <div className="input-container">
+            <label htmlFor="weight">Weight (Kg):</label>
+            <input type="text" value={weight} id='weight' onChange={(e)=>setWeight(e.target.value)}/>
+          </div>
+          <button onClick={bmicalculate}>Caluculate BMI</button>
+          <button onClick={clearAll}>Clear</button>
+          {bmi!==null &&(
+            <div className="result">
+            <p>Your BMI is: {bmi}</p>
+            <p>Status: {bmiStatus}</p>
+          </div>
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
